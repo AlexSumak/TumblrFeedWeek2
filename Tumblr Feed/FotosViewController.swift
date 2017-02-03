259155c7acc70b461c83c0444c25317b45b1ac40
Alex Sumak <alex.sumak@Alex-Sumaks-MacBook-Air.local>
2017-02-02 20:34:35 -0800

    new changes

---
+++ Tumblr Feed/FotosViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FotosViewController.swift
+//  Tumblr Feed
+//
+//  Created by  Alex Sumak on 2/2/17.
+//  Copyright © 2017  Alex Sumak. All rights reserved.
+//
+
+import UIKit
+
+class FotosViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
