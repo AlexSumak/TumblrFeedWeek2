d2530c411e5aa1dca58de5bad86483cfc7e8d984
Alex Sumak <alex.sumak@Alex-Sumaks-MacBook-Air.local>
2017-02-02 21:38:32 -0800

    done

---
+++ Tumblr Feed/fotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  fotoCell.swift
+//  Tumblr Feed
+//
+//  Created by  Alex Sumak on 2/2/17.
+//  Copyright © 2017  Alex Sumak. All rights reserved.
+//
+
+import UIKit
+
+class fotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
